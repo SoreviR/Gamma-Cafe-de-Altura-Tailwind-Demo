3296a12ebac703e2011fa9b4a9f3e7a1390c99d6
SoreviR <rob.riveros.es@gmail.com>
2023-11-03 00:03:54 +0100

    third commit - start index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        "Grey-color": "var(--Grey)",
+        "Dark-grey-color": "var(--Dark-grey)",
+        "Black-color": "var(--Black)",
+        "Taupe-color": "var(--Taupe)",
+        "Green-color": "var(--Green)",
+        "Off-white-color": "var(--Off-white)",
+        "White-color": "var(--White)",
+      },
       fontFamily:{
         body: ['Outfit', "sans-serif"]
       }
